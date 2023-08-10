5a35e7989d2b81ab1ca466684f41788e9fb247ca
Badretdinov Bulat <badretdinovbr@gmail.com>
2023-08-10 21:29:12 +0500

    added HW2_Ex10

---
+++ HW2_Ex10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите целое трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int temp = number / 10;
+int SecondDigit = temp % 10;
+
+Console.WriteLine(SecondDigit);
