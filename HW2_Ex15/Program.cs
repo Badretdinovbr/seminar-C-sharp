1f13b42f577a06194e10cb67997df06d479d71e8
Badretdinov Bulat <badretdinovbr@gmail.com>
2023-08-11 19:28:29 +0500

    added Hw2_Ex15

---
+++ HW2_Ex15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+int Message(string msg)
+{
+    Console.WriteLine($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Message("Введите день недели:");
+string week = "Это не день недели";
+
+string check(int number)
+{
+    if (number >= 6 && number < 8) week = "Да";
+    if (number >= 1 && number < 6) week = "Нет";
+    return week;
+}
+
+Console.WriteLine(check(number));
